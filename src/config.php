961c7558081fbc485afd209a2919920728784436
Elise <elise.labbe@gmail.com>
2021-07-14 04:44:08 +0200

    Ajout param recupération code auth + envoie token

---
+++ src/config.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'oauthtwitch.php';
+$oauth = new OAuthTwitch('m62ejysxqzfv1pggkc0er7q4pc05rt', 'zyk28wmnyrqc48vpstwon0h7lhgkt2', 'http://localhost/PointBot/', 'user:read:broadcast');
+?>
